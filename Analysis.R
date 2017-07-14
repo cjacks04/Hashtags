0b295558772dc55e430150b84763798df4cba01b
Corey Jackson <cjacks04@syr.edu>
2017-07-14 13:00:08 -0500

    Update Analysis.R

--- Analysis.R
+++ Analysis.R
@@ -1,1 +1,19 @@
 # Hashtag Analysis
+# Philip Lee and Corey Jackson 2017
+
+# Load Libraries 
+library(reshape2)
+
+# Import hashtag dataset
+# File is here: https://www.dropbox.com/s/cdil6uu5lcrucmf/hashtags_format.csv?dl=0
+
+hashtags <- read.csv("")
+
+# Create dataframe to get columns unique tag, date of first use, user_name, use count
+hashtags_introduced <- 
+
+# Create subset of tags using hashtags_introduced date > oct 12 (launch) and date < june 14 (1 mth before last observation) and use count > 1
+hashtag_population <- 
+
+
+
