62f9d3b54ec7e9a0010d308eee087830c7551133
Corey Jackson <cjacks04@syr.edu>
2017-07-14 12:45:53 -0500

    Get json and make csv

---
+++ json-to-csv.R
@@ -0,0 +1,35 @@
+# Corey Jackson
+# JSON to CSV for Zoonvierse Hashtag file
+
+
+library(RJSONIO)
+library(reshape)
+library(reshape2)
+library(dplyr)
+library(tidyjson)  #https://github.com/sailthru/tidyjson
+library(rjson)
+library(readr)
+library(plyr)
+
+# Import file
+hashtags <- read_file("~/Dropbox/INSPIRE/REU Projects/Hashtag Use/Datasets/Gravity Spy/project-1104-tags_2017-03-26.json")
+
+# Take json to csv  
+hashtags_format <- hashtags %>% as.tbl_json %>% 
+    gather_array %>% 
+     spread_values(
+    id = jstring("id"),
+    tag = jstring("name"),
+    user = jstring("user_login"),
+    user_id = jstring("user_id"),
+    created_at = jstring("created_at"),
+    type = jstring("taggable_type"),
+    comment_id = jstring("comment_id"),
+    taggable_id = jstring("taggable_id")
+    ) 
+
+remove(hashtags)
+
+# Export file to Dropbox
+setwd("~/Dropbox/INSPIRE/REU Projects/Hashtag Use/Datasets/Gravity Spy")
+write.csv(hashtags_format, "hashtags_format.csv")
